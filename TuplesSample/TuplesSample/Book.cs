bc9bd77c6d08b748d02d2d8cd27466d06e518b10
Christian Nagel <christian@christiannagel.com>
2018-03-15 13:37:19 +0100

    tuples

---
+++ TuplesSample/TuplesSample/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuplesSample
+{
+    public class Book
+    {
+        public Book(string title, string publisher)
+        {
+            Title = title;
+            Publisher = publisher;
+        }
+        public string Title { get; }
+        public string Publisher { get; }
+    }
+
+    public static class BookExtensions
+    {
+        public static void Deconstruct(this Book book, out string title, out string publisher)
+        {
+            title = book.Title;
+            publisher = book.Publisher;
+        }
+    }
+}
---
+++ TuplesSample/TuplesSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TuplesSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample1();
+            Deconstruction();
+        }
+
+        private static void Deconstruction()
+        {
+            var b1 = new Book("Professional C# 7", "Wrox Press");
+            (string title, _) = b1;
+            Console.WriteLine(title);
+        }
+
+        private static void Sample1()
+        {
+            var t1 = Tuple.Create(42, "abc");
+            // t1.Item1 = 11;  // immutable
+
+
+            var t2 = (x: 42, s: "abc");  // tuple
+            t2.x = 11;
+
+            int a = 3;
+            a = 4;
+
+            (int x, string s) = t2;  // deconstruction
+            
+        }
+    }
+}
